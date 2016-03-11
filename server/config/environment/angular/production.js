c556033bde004fdcc7c0bdbfcd22b308f28f2738
Manjesh Vinayaka <manjesh@quetzal.in>
2016-03-11 17:09:20 +0530

    Production Server Settings QAPI

--- server/config/environment/angular/production.js
+++ server/config/environment/angular/production.js
@@ -1,8 +1,8 @@
 'use strict';
 
 exports = module.exports = {
-  QUARC_API: 'https://api.cloud.quezx.com/api',
-  PARTNER_OAUTH_API:'https://api.cloud.quezx.com/applications/partner/api',
+  QUARC_API: 'https://qapi.quezx.com/api',
+  PARTNER_OAUTH_API:'https://qapi.quezx.com/applications/partner/api',
   ACCOUNTS: '//accounts.quezx.com',
   OAUTH: '//accounts.quezx.com/authorise?client_id=partnerquezx&response_type=code&' +
   'redirect_uri=https://partner.quezx.com/access/oauth&state=yo',
